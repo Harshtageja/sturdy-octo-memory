9be87f41f41ef24e3f1de734d54ec42dc8308097
Harshtageja <61935490+Harshtageja@users.noreply.github.com>
2020-12-22 10:28:50 +0530

    Add files via upload

---
+++ array transpose.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[5][5]={
+		{
+			1,2,3,4,5
+		},{
+		6,7,8,9,10
+		},{
+		11,12,13,14,15
+		},{
+			16,17,18,19,20
+		},{
+			21,22,23,24,25
+		}
+	};
+	int i,j;
+	for(i=0;i<5;i++){
+		for(j=0;j<=i;j++){
+			swap(a[i][j],a[j][i]);
+		}
+	}
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			cout<<a[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
