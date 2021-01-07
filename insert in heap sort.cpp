1aa67150db5a7fbcf49e83906c81c7184fab4763
Harshtageja <61935490+Harshtageja@users.noreply.github.com>
2021-01-07 22:03:12 +0530

    Add files via upload

--- insert in heap sort.cpp
+++ insert in heap sort.cpp
@@ -12,6 +12,34 @@ void ins(int a[],int i,int d){
 			p=(i-1)/2;
 	}
 }
+void remove(int a[],int n){
+	a[0]=a[n-1];
+	int i=0;
+	int lc=2*i+1;
+	int rc=2*i+2;
+	while((a[i]>a[lc]||a[i]>a[rc])&&lc<n-1){
+		if(a[i]>a[lc]&&a[i]>rc){
+			if(a[lc]>a[rc]){
+				swap(a[i],a[rc]);
+				i=rc;
+			}
+			else{
+				swap(a[i],a[lc]);
+				i=lc;
+			}
+		}
+		else if(a[i]>a[lc]){
+			swap(a[i],a[lc]);
+			i=lc;
+		}
+		else{
+			swap(a[i],a[rc]);
+			i=rc;
+		}
+		lc=2*i+1;
+		 rc=2*i+2;
+	}
+}
 int main(){
 	int a[5];
 	ins(a,0,23);
@@ -20,7 +48,10 @@ int main(){
 	ins(a,3,66);
 	ins(a,4,3);
 	int i;
-	for(i=0;i<5;i++){
+	remove(a,5);
+	remove(a,4);
+	remove(a,3);
+	for(i=0;i<3;i++){
 		cout<<a[i]<<"\n";
 	}
 	return 0;
