f4e70a96c9bb21e20f47b9d2c25a041eae656ab8
Harshtageja <61935490+Harshtageja@users.noreply.github.com>
2021-02-15 13:13:37 +0530

    Add files via upload

---
+++ sqlday1.cpp
@@ -0,0 +1,5 @@
+Select distinct *from customers where not id=5 order by id;
+insert into customers values(1,"harshk","harsh");
+Is NuLl
+update customers set name='kk' where id=5;
+select *from customers where rownum<3;
